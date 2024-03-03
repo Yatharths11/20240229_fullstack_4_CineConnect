7149e6668c62fc6862920892dbab166417797206
Levi Ackerman <umeshshelare@gmail.com>
2024-03-04 01:30:36 +0530

    update in schema

---
+++ 05_Backend/schema/movies.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+
+const movieSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+
+  venue_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Venue",
+    required: true,
+  },
+
+  description: {
+    type: String,
+  },
+
+  screen: {
+    type: String,
+    required: true,
+    default: "Audi_01",
+  },
+
+  language: {
+    type: String,
+    required: true,
+  },
+
+  genre: {
+    type: [String],
+    required: true,
+  },
+
+  price: {
+    type: Number,
+    required: true,
+    min: 50,
+  },
+
+  ratings: {
+    type: Number,
+    required: true,
+  },
+
+  date: {
+    type: Date,
+    required: true,
+  },
+
+  availableSeats: {
+    type: Number,
+    min: 0,
+  },
+
+  pgRating: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Movie", movieSchema);

782ada35f35f8a819f1290c1a78932a2420eecfd
AyanS1912 <92726765+AyanS1912@users.noreply.github.com>
2024-03-04 00:12:41 +0530

    Resolving conflicts

--- 05_Backend/schema/movies.js
+++ 05_Backend/schema/movies.js
@@ -7,7 +7,7 @@ const movieSchema = new mongoose.Schema({
         required: true 
     },
 
-    theatre_id: {
+    venue_id: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'Theatre',
         required: true

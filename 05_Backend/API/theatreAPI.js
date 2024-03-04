0c7ebc07976a316c2d633473b836879af37276d9
AyanS1912 <92726765+AyanS1912@users.noreply.github.com>
2024-03-04 23:28:56 +0530

    Response format is restructured.

--- 05_Backend/API/theatreAPI.js
+++ 05_Backend/API/theatreAPI.js
@@ -6,10 +6,6 @@ const Theatres = require('../schema/theatre.js')
 const Movies = require('../schema/movies.js')
 
 
-
-router.use(express.urlencoded({ extended: true })) // Parse URL-encoded bodies (as sent by HTML forms)
-router.use(express.json())
-
 router.use((req, res, next) => {
     const token = req.headers.authorization && req.headers.authorization.split(' ')[1]
     if (!token) {
@@ -28,48 +24,39 @@ router.use((req, res, next) => {
     }
   })
 
-//API to get all the list of theatres and their details
-router.get("/",async (req,res)=>{
+// API to get all the list of theatres and their details
+router.get("/", async (req, res) => {
     try {
-        let theatres = await Theatres.find()
+        const theatres = await Theatres.find()
         res.status(200).json(theatres)
     } catch (err) {
-        console.error("Error in retrieving data:", err)
+        console.error("Error retrieving data:", err)
         res.status(500).json({ error: "Internal Server Error" })
     }
 })
 
 
 
-//API to get details of a particular theatre
-router.get('/:id',(req,res)=>{
-    try{
-        const theatre = Theatres.find({_id : req.query.id})
-        .then(result=>{
-            if(!result){
-                return res.status(404).json({message:"theatre not found."})
-            }else{
-              return res.status(200).json(result[0])
-            }
-          })
-          .catch(e =>{
-            console.log(e)
-            res.status(500).json({error: e})
-          })
-    }
-    catch(err){
-        res.status(501).json(err)
+// API to get details of a particular theatre
+router.get('/:id', async (req, res) => {
+    try {
+        const theatre = await Theatres.findById(req.params.id)
+        if (!theatre) {
+            return res.status(404).json({ message: "Theatre not found." })
+        }
+        res.status(200).json(theatre)
+    } catch (err) {
+        console.error("Error retrieving theatre details:", err)
+        res.status(500).json({ error: "Internal Server Error" })
     }
 })
 
 
-//API to create a new theatres
-// Create a new theatre
+// API to create a new theatre
 router.post('/', async (req, res) => {
     const theatre = req.body
-
     try {
-        const newTheatre = await createTheatre(theatre)
+        const newTheatre = await Theatres.create(theatre)
         res.status(201).json(newTheatre)
     } catch (err) {
         console.error("Error creating theatre:", err)
@@ -77,63 +64,49 @@ router.post('/', async (req, res) => {
     }
 })
 
-// Function to create a new theatre
-async function createTheatre(theatreData) {
-    try {
-        const newTheatre = await Theatres.create(theatreData)
-        return newTheatre
-    } catch (err) {
-        console.error("Error creating theatre:", err)
-        throw err // Re-throw the error to be caught in the calling code
-    }
-}
 
-router.delete('/:id',async (req,res)=>{
-    let id=req.params.id
+// API to delete a theatre by ID
+router.delete('/:id', async (req, res) => {
+    const id = req.params.id
     try {
-        const result = await Theatres.deleteOne({ _id: id }).exec()
+        const result = await Theatres.deleteOne({ _id: id })
         if (result.deletedCount === 0) {
             return res.status(404).json({ message: 'No data with that ID' })
         }
-        res.status(200).json({ message: 'theatre deleted successfully' })
+        res.status(200).json({ message: 'Theatre deleted successfully' })
     } catch (err) {
         console.error("Error deleting theatre:", err)
         res.status(500).json({ error: "Internal Server Error" })
     }
 })
 
-router.get("/:id/movies",async (req,res)=>{
-    try{
-        const movies = await Movies.findOne({theatre_id:req.params.id})
-        .then((data)=>{
-            if(!data){
-                res.status(200).json(`There are no movies showning at this theater right now.\n Please come back again`)
-            }
-            else{
-                res.status(200).json(data)
-            }
-        })
-    }
-    catch(err){
-        res.status(501).json(err)
+// API to get movies of a particular theatre
+router.get("/:id/movies", async (req, res) => {
+    try {
+        const movies = await Movies.findOne({ theatre_id: req.params.id })
+        if (!movies) {
+            res.status(200).json(`There are no movies showing at this theatre right now.\nPlease come back again`)
+        } else {
+            res.status(200).json(movies)
+        }
+    } catch (err) {
+        console.error("Error retrieving movies:", err)
+        res.status(500).json({ error: "Internal Server Error" })
     }
 })
 
-
-router.put( "/:id", async (req,res)=>{
+// API to update a theatre by ID
+router.put("/:id", async (req, res) => {
     const theaterId = req.params.id
-
     try {
         const updatedTheatre = await Theatres.findByIdAndUpdate(
             theaterId,
             req.body, // Update with the data provided in req.body
             { new: true, runValidators: true }
         )
-
         if (!updatedTheatre) {
             return res.status(404).json({ message: 'Theater not found' })
         }
-
         res.status(200).json(updatedTheatre)
     } catch (err) {
         console.error("Error updating theatre:", err)

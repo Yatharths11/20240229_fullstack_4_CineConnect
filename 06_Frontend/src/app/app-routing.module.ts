fe8b857a672e2708bd6637d9d9a0904e2217fea2
AyanS1912 <92726765+AyanS1912@users.noreply.github.com>
2024-03-11 15:36:54 +0530

    Edit Profile and Payment Page updated

--- 06_Frontend/src/app/app-routing.module.ts
+++ 06_Frontend/src/app/app-routing.module.ts
@@ -1,12 +1,31 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-// import { ForgotpasswordComponent } from './forgotpassword/forgotpassword.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+import { CreateShowComponent } from './create-show/create-show.component';
+import { CreateTheatreComponent } from './create-theatre/create-theatre.component';
+import { ForgotPasswordComponent } from './forgot-password/forgot-password.component'
+import { SignupComponent } from './signup/signup.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
+import { ManageTheatreComponent } from './manage-theatre/manage-theatre.component';
+import { ManageShowsComponent } from './manage-shows/manage-shows.component';
+import { PaymentComponent } from './payment/payment.component';
+import { EditProfileComponent } from './edit-profile/edit-profile.component';
 
 const routes: Routes = [
 
-  // { path: '/', component: RegisterLoginComponent },
-  // { path: '/forgot-password', component : ForgotpasswordComponent}
-  // { path: '', redirectTo: '/login', pathMatch: 'full' }
+  { path: 'login', component: LoginComponent },
+  { path: 'home', component : HomeComponent},
+  { path: 'create-show', component: CreateShowComponent },
+  { path: 'create-theatre', component: CreateTheatreComponent },
+  { path: 'forgot-password', component: ForgotPasswordComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'manage-theatre', component: ManageTheatreComponent },
+  { path: 'manage-shows', component: ManageShowsComponent },
+  { path: 'payment', component: PaymentComponent },
+  { path: 'edit-profile', component: EditProfileComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirect to login if no matching route
+  { path: '**', component: PagenotfoundComponent } // Redirect to page not found for unknown routes
 
 ];
 
--- 06_Frontend/src/app/payment/payment.component.ts
+++ 06_Frontend/src/app/payment/payment.component.ts
@@ -1,51 +1,175 @@
-import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Component ,  OnInit, ElementRef, Renderer2  } from '@angular/core';
+// import { FormGroup, FormControl, Validators  } from '@angular/forms';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
   styleUrl: './payment.component.css'
 })
-export class PaymentComponent {
+export class PaymentComponent  implements OnInit  {
 
-  paymentForm = new FormGroup({
-    name: new FormControl('', [
-      Validators.required,
-      Validators.maxLength(30),
-      Validators.pattern('^[a-zA-Z ]*$') // Only alphabets and spaces allowed
-    ]),
-    email: new FormControl('', [
-      Validators.required,
-      Validators.email,
-      Validators.pattern('^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$')
-    ]),
-    expiryDate: new FormControl('', [
-      Validators.required,
-      Validators.pattern('^(0[1-9]|1[0-2])/([2-9][5-9]|[3-4][0-9]|50)$')
-    ]),
-    cvc: new FormControl('', [
-      Validators.required,
-      Validators.pattern('^[0-9]{3}$') // Exactly 3 digits
-    ]),
-    nameOnCard: new FormControl('', [
-      Validators.required,
-      Validators.pattern('^[a-zA-Z ]*$') // Only alphabets and spaces allowed
-    ]),
-    card: new FormControl('', [
-      Validators.required,
-      Validators.pattern('^(\d{4} ?){4}$') // 16 digits with groups of 4 separated by space
-    ])
-  });
+  constructor(private elRef: ElementRef, private renderer: Renderer2) { }
 
-  inputFocused: { [key: string]: boolean } = {};
-
-  onFocus(controlName: string) {
-    this.inputFocused[controlName] = true;
-    // console.log(controlName,this.inputFocused[controlName])
+  ngOnInit(): void {
+    this.init();
   }
 
-  onBlur(controlName: string) {
-    this.inputFocused[controlName] = false;
-    // console.log(controlName,this.inputFocused[controlName])
+  init() {
+    const d = this.elRef.nativeElement.ownerDocument;
+    const body = d.getElementsByTagName('body')[0];
+    const ppForm = d.getElementsByTagName('form')[0];
+    const ccForm = d.getElementsByTagName('form')[1];
+    const ecForm = d.getElementsByTagName('form')[2];
+    const cCard = d.querySelector('#cc-card');
+    const pCard = d.querySelector('#pp-card');
+    const eCard = d.querySelector('#ec-card');
+    const info = d.querySelector('#choosen-paymenttype');
+    const ccNumber = ccForm.querySelector('#cardnumber');
+    const cNumber = d.querySelectorAll('.card-number');
+    const ccName = ccForm.querySelector('#cardholder');
+    const cName = d.querySelectorAll('.card-holder');
+    const ccMonth = ccForm.querySelector('#expires-month');
+    const cMonth = d.querySelectorAll('.e-month');
+    const ccYear = ccForm.querySelector('#expires-year');
+    const cYear = d.querySelectorAll('.e-year');
+    const ccCCV = ccForm.querySelector('#ccv');
+    const cCCV = d.querySelector('.ccv strong');
+    const ccCard = d.querySelectorAll('.credit-card-type');
+    const defaultNumber = cNumber[0].getElementsByTagName('span')[0].innerHTML;
+    const defaultName = cName[0].getElementsByTagName('span')[0].innerHTML;
+
+    body.className = 'cc-bg';
+
+    const switchPos = (elm: HTMLElement) => {
+      if (elm.classList.contains('selected')) {
+        if (elm.getElementsByTagName('input').length) {
+          elm.getElementsByTagName('input')[0].focus();
+        }
+        return;
+      }
+      const selected = d.querySelector('.selected');
+
+      if (elm.classList.contains('unselected-left')) {
+        selected.classList.remove('selected');
+        selected.classList.add('unselected-left');
+        elm.classList.add('selected');
+        elm.classList.remove('unselected-left');
+        if (window.matchMedia("(max-width: 1039px)").matches) {
+          setTimeout(() => { elm.scrollIntoView(); }, 500);
+        }
+      } else if (elm.classList.contains('unselected-right')) {
+        selected.classList.remove('selected');
+        selected.classList.add('unselected-right');
+        elm.classList.add('selected');
+        elm.classList.remove('unselected-right');
+        if (window.matchMedia("(max-width: 1039px)").matches) {
+          setTimeout(() => { elm.scrollIntoView(); }, 500);
+        }
+      }
+    };
+
+    const addEvent = (elem: EventTarget, event: string, func: (event: Event) => void) => {
+      this.renderer.listen(elem, event, func);
+    };
+    
+   
+    const getCardType = (number: string) => {
+      let re;
+      // Mastercard
+      re = new RegExp("^5[1-5]");
+      if (number.match(re) != null) {
+        return "mastercard";
+      }
+      // AMEX
+      re = new RegExp("^3[47]");
+      if (number.match(re) != null) {
+        return "amex";
+      }
+
+      // visa
+      re = new RegExp("^4");
+      if (number.match(re) != null) {
+        return "visa";
+      }
+
+      return "";
+    };
+
+    const syncText = (elCol: NodeListOf<Element>, text: string) => {
+      let collection;
+      for (let j = 0; j < elCol.length; j++) {
+        collection = elCol[j].querySelectorAll('span');
+        if (!collection.length) {
+          elCol[j].innerHTML = text;
+        } else {
+          for (let i = 0; i < collection.length; i++) {
+            collection[i].innerHTML = text;
+          }
+        }
+      }
+    };
+
+    const numSplit = (number: string, indexes: number[]) => {
+      const tempArr = number.split('');
+      const parts = [];
+      for (let i = 0; i < indexes.length; i++) {
+        if (tempArr.length) {
+          parts.push(tempArr.splice(0, indexes[i]).join(''));
+        }
+      }
+      return parts;
+    };
+
+    addEvent(pCard, 'click', () => {
+      switchPos(d.querySelector('.paymenttype.pp'));
+      body.className = 'pp-bg';
+      info.innerHTML = 'PayPal';
+    });
+    addEvent(cCard, 'click', () => {
+      switchPos(d.querySelector('.paymenttype.cc'));
+      body.className = 'cc-bg';
+      info.innerHTML = 'Credit Card';
+    });
+    addEvent(eCard, 'click', () => {
+      switchPos(d.querySelector('.paymenttype.ec'));
+      body.className = 'ec-bg';
+      info.innerHTML = 'Bank account';
+    });
+
+    addEvent(ccNumber, 'keyup', () => {
+      let cardNumber = ccNumber.value.replace(/[^0-9\s]/g, '');
+      if (!!ccNumber.value.match(/[^0-9\s]/g)) {
+        ccNumber.value = cardNumber;
+      }
+      const cardType = getCardType(cardNumber.replace(/\s/g, ''));
+      let parts;
+      switch (cardType) {
+        case 'amex':
+          parts = numSplit(cardNumber.replace(/\s/g, ''), [4, 6, 5]);
+          ccCard[0].className = 'credit-card-type amex';
+          break;
+        case 'mastercard':
+          parts = numSplit(cardNumber.replace(/\s/g, ''), [4, 4, 4, 4]);
+          ccCard[0].className = 'credit-card-type mastercard';
+          break;
+        case 'visa':
+          parts = numSplit(cardNumber.replace(/\s/g, ''), [4, 4, 4, 4]);
+          ccCard[0].className = 'credit-card-type visa';
+          break;
+        default:
+          parts = cardNumber.split(' ');
+          ccCard[0].className = 'credit-card-type';
+      }
+      cardNumber = parts.join(' ');
+      if (cardNumber != ccNumber.value) {
+        ccNumber.value = cardNumber;
+      }
+      if (!cardNumber) {
+        cardNumber = defaultNumber;
+      }
+      syncText(cNumber, cardNumber);
+    });
+
+    // Add other event listeners and functionality similarly
   }
 }

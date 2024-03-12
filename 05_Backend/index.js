543e05754f8310b2ea16bc68547571d5206694c9
Mayuresh <mayureshngorantiwar@gmail.com>
2024-03-12 19:14:34 +0530

    merge

--- 05_Backend/index.js
+++ 05_Backend/index.js
@@ -1,5 +1,4 @@
 // Import required modules
-require("dotenv").config()
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
@@ -7,43 +6,6 @@ const dotenv = require("dotenv");
 
 // Load environment variables from .env file
 dotenv.config();
-const swaggerJSDoc = require('swagger-jsdoc');
-const swaggerUi = require('swagger-ui-express');
-
-const swaggerDefinition = {
-  openapi: '3.0.0',
-  info: {
-    title: 'Express API for JSONPlaceholder',
-    version: '1.0.0',
-    description:
-      'This is a REST API application made with Express. It retrieves data from JSONPlaceholder.',
-    license: {
-      name: 'Licensed Under MIT',
-      url: 'https://spdx.org/licenses/MIT.html',
-    },
-    contact: {
-      name: 'JSONPlaceholder',
-      url: 'https://jsonplaceholder.typicode.com',
-    },
-  },
-  servers: [
-    {
-      url: 'http://localhost:5000',
-      description: 'Development server',
-    },
-  ],
-};
-
-const options = {
-  swaggerDefinition,
-  // Paths to files containing OpenAPI definitions
-  apis: ['./API/*.js'],
-};
-
-const swaggerSpec = swaggerJSDoc(options);
-
-app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-
 
 // Create an Express app
 const app = express();

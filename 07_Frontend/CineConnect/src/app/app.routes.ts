d485a3eb45bb2419b77e166dd31bf3ac46eecfa6
Mayuresh <mayureshngorantiwar@gmail.com>
2024-03-12 15:52:10 +0530

    navbar

--- 07_Frontend/CineConnect/src/app/app.routes.ts
+++ 07_Frontend/CineConnect/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './Pages/home/home.component';
+import { PageNotFoundComponent } from './Pages/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:HomeComponent},
+    {path:'**',component:PageNotFoundComponent}
+];
